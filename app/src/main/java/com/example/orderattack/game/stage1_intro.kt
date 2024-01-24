00521c8f1d5c1c130eb66ab84ae0a94dbf00839c
Lee In Sun <105425832+leeinsunny@users.noreply.github.com>
2024-01-24 19:49:01 +0900

    before making game per stage (#7)
    각 단계별 게임 제작 직전까지 만들었습니다

---
+++ app/src/main/java/com/example/orderattack/game/stage1_intro.kt
@@ -0,0 +1,13 @@
+package com.example.orderattack.game
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.orderattack.R
+
+class stage1_intro: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stage1intro)
+
+    }
+}
