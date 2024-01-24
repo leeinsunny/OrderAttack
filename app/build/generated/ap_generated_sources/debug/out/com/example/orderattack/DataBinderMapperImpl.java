0d51e53283b53f761d7034f09384feb78d4f0219
Lee In Sun <105425832+leeinsunny@users.noreply.github.com>
2024-01-24 19:49:01 +0900

    before making game per stage (#7)
    각 단계별 게임 제작 직전까지 만들었습니다

--- app/build/generated/ap_generated_sources/debug/out/com/example/orderattack/DataBinderMapperImpl.java
+++ app/build/generated/ap_generated_sources/debug/out/com/example/orderattack/DataBinderMapperImpl.java
@@ -6,10 +6,14 @@ import android.view.View;
 import androidx.databinding.DataBinderMapper;
 import androidx.databinding.DataBindingComponent;
 import androidx.databinding.ViewDataBinding;
+import com.example.orderattack.databinding.ActivitiyStage2introBindingImpl;
+import com.example.orderattack.databinding.ActivityGameintroBindingImpl;
 import com.example.orderattack.databinding.ActivityIntroBindingImpl;
 import com.example.orderattack.databinding.ActivityJoinBindingImpl;
 import com.example.orderattack.databinding.ActivityLoginBindingImpl;
 import com.example.orderattack.databinding.ActivityMainBindingImpl;
+import com.example.orderattack.databinding.ActivityStage1introBindingImpl;
+import com.example.orderattack.databinding.ActivityStage3introBindingImpl;
 import java.lang.IllegalArgumentException;
 import java.lang.Integer;
 import java.lang.Object;
@@ -21,21 +25,33 @@ import java.util.HashMap;
 import java.util.List;
 
 public class DataBinderMapperImpl extends DataBinderMapper {
-  private static final int LAYOUT_ACTIVITYINTRO = 1;
+  private static final int LAYOUT_ACTIVITIYSTAGE2INTRO = 1;
 
-  private static final int LAYOUT_ACTIVITYJOIN = 2;
+  private static final int LAYOUT_ACTIVITYGAMEINTRO = 2;
 
-  private static final int LAYOUT_ACTIVITYLOGIN = 3;
+  private static final int LAYOUT_ACTIVITYINTRO = 3;
 
-  private static final int LAYOUT_ACTIVITYMAIN = 4;
+  private static final int LAYOUT_ACTIVITYJOIN = 4;
 
-  private static final SparseIntArray INTERNAL_LAYOUT_ID_LOOKUP = new SparseIntArray(4);
+  private static final int LAYOUT_ACTIVITYLOGIN = 5;
+
+  private static final int LAYOUT_ACTIVITYMAIN = 6;
+
+  private static final int LAYOUT_ACTIVITYSTAGE1INTRO = 7;
+
+  private static final int LAYOUT_ACTIVITYSTAGE3INTRO = 8;
+
+  private static final SparseIntArray INTERNAL_LAYOUT_ID_LOOKUP = new SparseIntArray(8);
 
   static {
+    INTERNAL_LAYOUT_ID_LOOKUP.put(com.example.orderattack.R.layout.activitiy_stage2intro, LAYOUT_ACTIVITIYSTAGE2INTRO);
+    INTERNAL_LAYOUT_ID_LOOKUP.put(com.example.orderattack.R.layout.activity_gameintro, LAYOUT_ACTIVITYGAMEINTRO);
     INTERNAL_LAYOUT_ID_LOOKUP.put(com.example.orderattack.R.layout.activity_intro, LAYOUT_ACTIVITYINTRO);
     INTERNAL_LAYOUT_ID_LOOKUP.put(com.example.orderattack.R.layout.activity_join, LAYOUT_ACTIVITYJOIN);
     INTERNAL_LAYOUT_ID_LOOKUP.put(com.example.orderattack.R.layout.activity_login, LAYOUT_ACTIVITYLOGIN);
     INTERNAL_LAYOUT_ID_LOOKUP.put(com.example.orderattack.R.layout.activity_main, LAYOUT_ACTIVITYMAIN);
+    INTERNAL_LAYOUT_ID_LOOKUP.put(com.example.orderattack.R.layout.activity_stage1intro, LAYOUT_ACTIVITYSTAGE1INTRO);
+    INTERNAL_LAYOUT_ID_LOOKUP.put(com.example.orderattack.R.layout.activity_stage3intro, LAYOUT_ACTIVITYSTAGE3INTRO);
   }
 
   @Override
@@ -47,6 +63,18 @@ public class DataBinderMapperImpl extends DataBinderMapper {
         throw new RuntimeException("view must have a tag");
       }
       switch(localizedLayoutId) {
+        case  LAYOUT_ACTIVITIYSTAGE2INTRO: {
+          if ("layout/activitiy_stage2intro_0".equals(tag)) {
+            return new ActivitiyStage2introBindingImpl(component, view);
+          }
+          throw new IllegalArgumentException("The tag for activitiy_stage2intro is invalid. Received: " + tag);
+        }
+        case  LAYOUT_ACTIVITYGAMEINTRO: {
+          if ("layout/activity_gameintro_0".equals(tag)) {
+            return new ActivityGameintroBindingImpl(component, view);
+          }
+          throw new IllegalArgumentException("The tag for activity_gameintro is invalid. Received: " + tag);
+        }
         case  LAYOUT_ACTIVITYINTRO: {
           if ("layout/activity_intro_0".equals(tag)) {
             return new ActivityIntroBindingImpl(component, view);
@@ -70,6 +98,18 @@ public class DataBinderMapperImpl extends DataBinderMapper {
             return new ActivityMainBindingImpl(component, view);
           }
           throw new IllegalArgumentException("The tag for activity_main is invalid. Received: " + tag);
+        }
+        case  LAYOUT_ACTIVITYSTAGE1INTRO: {
+          if ("layout/activity_stage1intro_0".equals(tag)) {
+            return new ActivityStage1introBindingImpl(component, view);
+          }
+          throw new IllegalArgumentException("The tag for activity_stage1intro is invalid. Received: " + tag);
+        }
+        case  LAYOUT_ACTIVITYSTAGE3INTRO: {
+          if ("layout/activity_stage3intro_0".equals(tag)) {
+            return new ActivityStage3introBindingImpl(component, view);
+          }
+          throw new IllegalArgumentException("The tag for activity_stage3intro is invalid. Received: " + tag);
         }
       }
     }
@@ -124,13 +164,17 @@ public class DataBinderMapperImpl extends DataBinderMapper {
   }
 
   private static class InnerLayoutIdLookup {
-    static final HashMap<String, Integer> sKeys = new HashMap<String, Integer>(4);
+    static final HashMap<String, Integer> sKeys = new HashMap<String, Integer>(8);
 
     static {
+      sKeys.put("layout/activitiy_stage2intro_0", com.example.orderattack.R.layout.activitiy_stage2intro);
+      sKeys.put("layout/activity_gameintro_0", com.example.orderattack.R.layout.activity_gameintro);
       sKeys.put("layout/activity_intro_0", com.example.orderattack.R.layout.activity_intro);
       sKeys.put("layout/activity_join_0", com.example.orderattack.R.layout.activity_join);
       sKeys.put("layout/activity_login_0", com.example.orderattack.R.layout.activity_login);
       sKeys.put("layout/activity_main_0", com.example.orderattack.R.layout.activity_main);
+      sKeys.put("layout/activity_stage1intro_0", com.example.orderattack.R.layout.activity_stage1intro);
+      sKeys.put("layout/activity_stage3intro_0", com.example.orderattack.R.layout.activity_stage3intro);
     }
   }
 }
---
+++ app/src/main/java/com/example/orderattack/game/stage2_intro.kt
@@ -0,0 +1,12 @@
+package com.example.orderattack.game
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.orderattack.R
+
+class stage2_intro: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activitiy_stage2intro)
+    }
+}
