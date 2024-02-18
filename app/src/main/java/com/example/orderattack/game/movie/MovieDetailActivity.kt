040cc5b68fd05146a594838378eeb1a3fdaec233
Minkyu Shin <digi1k2001@gmail.com>
2024-02-19 00:13:37 +0900

    movie

---
+++ app/src/main/java/com/example/orderattack/game/movie/MovieDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.orderattack.game.movie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.orderattack.R
+
+class MovieDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
---
+++ app/src/main/java/com/example/orderattack/game/movie/Step1ClearActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.orderattack.game.movie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.orderattack.R
+
+class Step1ClearActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step1_clear2)
+    }
+}
